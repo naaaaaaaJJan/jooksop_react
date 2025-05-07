b99d787769ace9d781253edc8ce684d4dc3d65e9
ynna <yoonna040321@naver.com>
2025-05-07 18:14:06 +0900

    윤나연합침

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import './App.css'
+import Signup from './pages/Signup';
+import './App.css';
 
 function App() {
   const isLoggedIn = true;
@@ -10,6 +11,7 @@ function App() {
   return(
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
       <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
