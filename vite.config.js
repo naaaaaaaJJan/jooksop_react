8d06e196d1b976eee5c5a7db0a24fad5302881a9
naaaaaaaJJan <ys155409@gmail.com>
2025-06-09 20:08:53 +0900

    0609

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/sw-project-fe/', 
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Spring Boot 백엔드 주소
+        target: 'http://localhost:8080', 
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
