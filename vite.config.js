c4a75d7d7f9279eea230139e0eb6ea2c63518542
naaaaaaaJJan <ys155409@gmail.com>
2025-06-10 10:58:20 +0900

    0610

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/sw-project-fe/', 
+  base: '/jooksope_react/', 
   plugins: [react()],
   build: {
     outDir: 'build',
